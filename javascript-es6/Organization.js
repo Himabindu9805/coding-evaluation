ff17f8c448fb94254bfef734ff69dcf7c6fdf2dd
Himabindu9805 <himabindu9805@gmail.com>
2022-11-06 11:30:36 -0600

    myhire changes js es6

--- javascript-es6/Organization.js
+++ javascript-es6/Organization.js
@@ -1,4 +1,7 @@
+import Position from  "./Position.js";
+import Employee from "./Employee.js";
 class Organization {
+
   constructor(root) {
     this.printOrganization = (position, prefix) => {
       let str = `${prefix}+-${position.toString()}\n`;
@@ -8,10 +11,32 @@ class Organization {
       return str;
     };
 
+    this.getPositions = (position,a)=>{
+      a.push(position)
+      for(let p of position.getDirectReports()){
+        this.getPositions(p,a);
+      }
+      return a;
+    }
+
     // Hire the given person as an employee in the position that has that title
     // Return the newly filled position or undefined if no position has that title
+
+
+
+
     this.hire = (person, title) => {
-      // your code here
+
+      let positions = this.getPositions(root,[]);
+      for(let i = 0;i<positions.length;i++){
+        if(positions[i].getTitle()==title){
+          let emp = new Employee(title,person)
+          positions[i].setEmployee(emp);
+          return positions[i];
+        }
+      }
+
+      return undefined;
     };
 
     this.toString = () => this.printOrganization(root, '');
